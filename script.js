0faf556d3bf37499594ebdb97e4316d6e2140a8e
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-14 10:59:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 // Complete the js code
 function Car(make, model) {
-	constructor(make, model){
 		this.make = make;
 		this.model = model;
-	}
-
-	getMakeModel(){
-		return this.make + " " + this.model;
-	}
 }
 
-function SportsCar(make, model, topSpeed) extends Car {
-	constructor(make, model, topSpeed){
-		super(make, model);
+Car.prototype.getMakeModel = function(){
+		return this.make + " " + this.model;
+	}
+
+function SportsCar(make, model, topSpeed) {
+		Car.call(this, make, model);
 		this.topSpeed = topSpeed;
 	}
 
-	getTopSpeed(){
+SportsCar.prototype.getTopSpeed = function(){
 		return this.topSpeed;
 	}
-}
+
+SportsCar.prototype = Object.create(Car.prototype)
+SportsCar.prototype.constructor = SportsCar;
 
 // Do not change the code below
 window.Car = Car;
