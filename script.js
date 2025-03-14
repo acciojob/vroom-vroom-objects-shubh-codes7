71898818cf7380f8cc7bb4c313558f343c29bf87
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-14 11:02:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ function SportsCar(make, model, topSpeed) {
 		this.topSpeed = topSpeed;
 	}
 
+SportsCar.prototype = Object.create(Car.prototype)
+SportsCar.prototype.constructor = SportsCar;
+
 SportsCar.prototype.getTopSpeed = function(){
 		return this.topSpeed;
 	}
-
-SportsCar.prototype = Object.create(Car.prototype)
-SportsCar.prototype.constructor = SportsCar;
 
 // Do not change the code below
 window.Car = Car;
